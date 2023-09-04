d58b1f2af6b6193679c696d9582c4ce26f8eecf7
0___dnjs <yelimim88@gmail.com>
2023-09-04 17:35:39 +0900

    feat: "2023/09/04"

--- src/main/java/sevlet/SignupServlet.java
+++ src/main/java/sevlet/SignupServlet.java
@@ -1,6 +1,7 @@
 package sevlet;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import data.UserData;
+import entity.User;
 import utils.JsonParseUtil;
 import utils.ResponseUtil;
 
@@ -27,6 +30,22 @@ public class SignupServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Map<String, Object> userMap = JsonParseUtil.toMap(request.getInputStream());
+		
+		System.out.println(userMap);
+		
+		List<User> userList = UserData.userList;
+		
+		User user = User.builder()
+				.userId(userList.size() + 1)
+				.username((String) userMap.get("username"))
+				.password((String) userMap.get("password"))
+				.name((String) userMap.get("name"))
+				.email((String) userMap.get("email"))
+				.build();
+		
+		userList.add(user);
+		
+		ResponseUtil.response(response).of(201).body(true);
 		
 //		InputStream inputStream = request.getInputStream();
 //		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -52,9 +71,9 @@ public class SignupServlet extends HttpServlet {
 //		System.out.println(userMap.get("name"));
 //		System.out.println(userMap.get("email"));ㄴ
 		
-		System.out.println("회원가입");
-		
-		ResponseUtil.response(response).of(200).body("회원가입 성공");
+//		System.out.println("회원가입");
+//		
+//		ResponseUtil.response(response).of(200).body("회원가입 성공");
 	}
 
 }
